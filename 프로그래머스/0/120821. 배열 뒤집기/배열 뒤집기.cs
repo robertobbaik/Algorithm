c022a7ab2647a20e6cd6c4698956ba17231566fe
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2024-12-25 19:55:47 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.22 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int len = num_list.Length % 2 == 0 ? len = num_list.Length / 2 : len = num_list.Length / 2 + 1;
+        
+        int[] answer = new int[num_list.Length];
+        
+        for(int i = 0; i < len; i++)
+        {
+            int temp = 0;
+            int swapIdx = (num_list.Length - 1) - i;
+            if(swapIdx == i)
+            {
+                answer[i] = num_list[i];
+            }
+            else
+            {
+                 temp = num_list[i];
+            answer[i] = num_list[swapIdx];
+            answer[swapIdx] = temp;
+            }
+           
+        }
+        
+        return answer;
+    }
+}
