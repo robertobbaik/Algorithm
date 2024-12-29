fa5596ee1a3bb924c1c7a1a73ab5f30f03e3ec5b
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2024-12-29 20:57:04 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    
+    string answer = "";
+    
+    int len = s.size();
+    
+    if(len % 2 == 0)
+    {
+        answer = s.substr(len /2 - 1, 2);
+    }
+    else
+    {
+        answer = s.substr(len / 2, 1);
+    }
+    
+    
+    return answer;
+}
