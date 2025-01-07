e944e13e17698caddec634d7f94f49b1a49a26b5
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-01-07 11:12:29 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -6,48 +6,25 @@ using namespace std;
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     string answer = "Yes";
     
-    vector<string> temp1;
-    vector<string> temp2;
+    int c1 = 0;
+    int c2 = 0;
+
     for (int i = 0; i < goal.size(); i++)
     {
-        for(int j = 0; j < cards1.size(); j++)
+        if(goal[i] == cards1[c1])
         {
-            if(goal[i] == cards1[j])
-            {
-                temp1.push_back(goal[i]);
-            }
+            c1++;
+            
         }
-
-             for(int j = 0; j < cards2.size(); j++)
+        else if(goal[i] == cards2[c2])
         {
-            if(goal[i] == cards2[j])
-            {
-                temp2.push_back(goal[i]);
-            }
+            c2++;
+        }
+        else
+        {
+            return "No";
         }
     }
 
-    for(int i = 0; i < temp1.size(); i++)
-    {
-        if(cards1[i] != temp1[i])
-        {
-            answer = "No";
-            break;
-        }
-    }
-
-    
-    for(int i = 0; i < temp2.size(); i++)
-    {
-        if(cards2[i] != temp2[i])
-        {
-            answer = "No";
-            break;
-        }
-    }
-    
-
-
-    
     return answer;
 }
