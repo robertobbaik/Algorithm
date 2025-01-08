2d9db13cd0f02f6f6a4d328e46ed59b749f59f6d
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-01-08 10:55:17 +0900

    [level 1] Title: 과일 장수, Time: 33.70 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    
+    sort(score.begin(), score.end(), greater<int>());
+    
+    for(int i = m; i <= score.size(); i += m)
+    {
+        answer += score[i - 1] * m;
+    }
+    
+    return answer;
+}
