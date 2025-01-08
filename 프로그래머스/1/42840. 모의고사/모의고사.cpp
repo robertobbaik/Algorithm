de5e4d0406d30a3033da381b5b156dca4a20b168
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-01-08 11:35:20 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+
+#include <algorithm>
+
+using namespace std;
+
+bool compare(const pair<int, int>& a, const pair<int, int>& b)
+{
+    if(a.second == b.second)
+        return a.first < b.first;
+    
+    return a.second < b.second;
+}
+    
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    vector<int> scores(3, 0);
+    
+    vector<vector<int>> temp = {{1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}}; 
+    
+    for(int i = 0; i < answers.size(); i++)
+    {
+        if(answers[i] == temp[0][i % 5])
+        {
+            scores[0]++;
+        }
+        
+        if(answers[i] == temp[1][i % 8])
+        {
+            scores[1]++;
+        }
+        
+        if(answers[i] == temp[2][i % 10])
+        {
+            scores[2]++;
+        }
+    }
+    
+    int maxScore = *max_element(scores.begin(), scores.end());
+    
+    for(int i = 0; i < scores.size(); i++)
+    {
+        if(scores[i] == maxScore)
+        {
+            answer.push_back(i + 1);
+        }
+    }
+        
+    return answer;
+}
