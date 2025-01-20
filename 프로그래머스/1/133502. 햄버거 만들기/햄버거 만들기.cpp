671ffa668df4e7c8245e020bccdf3ac4e6c02f4a
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-01-20 09:52:02 +0900

    [level 1] Title: 햄버거 만들기, Time: 12.52 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    
+    vector<int> stack;
+    for(int i : ingredient)
+    {
+        stack.push_back(i);
+
+        if(stack.size() >= 4 &&
+        stack[stack.size()-4] == 1 &&
+        stack[stack.size()-3] == 2 &&
+        stack[stack.size()-2] == 3 &&
+        stack[stack.size()-1] == 1)
+        {
+            stack.erase(stack.end() - 4 , stack.end());
+            ++answer;
+        }
+    }
+    return answer;
+}
