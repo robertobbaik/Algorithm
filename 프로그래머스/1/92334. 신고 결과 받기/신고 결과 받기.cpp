c17537e276170720eab31cf4093c282d5efd8a5f
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-01-25 17:20:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 280.84 ms, Memory: 39.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unordered_map>
+#include <set>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    set<string> reportset;
+    vector<int> answer;
+
+    unordered_map<string, int> reportCount;
+    unordered_map<string, set<string>> privatereport;
+
+    for(const string& name : id_list)
+    {
+        reportCount[name] = 0;
+    }
+
+    for(const string& plaint : report)
+    {
+        int blank = plaint.find(' ');
+        string complainant = plaint.substr(0, blank);
+        string defendant = plaint.substr(blank + 1, plaint.size() - blank);
+
+        privatereport[complainant].insert(defendant);
+    }
+
+    for(auto p : privatereport)
+    {
+        for (auto a : p.second)
+        {
+            ++reportCount[a];
+        }
+    }
+
+    for(auto temp : reportCount)
+    {
+        if(temp.second >= k)
+        {
+            reportset.insert(temp.first);
+        }
+    }
+
+    for(const string& name : id_list)
+    {
+        int reportCount = 0;
+        for(const string& temp : privatereport[name])
+        {
+            if(reportset.find(temp) != reportset.end())
+            {
+                ++reportCount;
+            }
+        }
+        answer.push_back(reportCount);
+    }
+    return answer;
+}
