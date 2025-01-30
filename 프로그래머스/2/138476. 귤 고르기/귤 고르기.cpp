bae78a739257b1c6041c10ea6877b820a74e9f53
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-01-30 23:09:59 +0900

    [level 2] Title: 귤 고르기, Time: 23.91 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,42 +1,9 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
 
 using namespace std;
 
 int solution(int k, vector<int> tangerine) {
-      int answer = 0;
-
-    unordered_map<int, int> tangerine_map;
-
-    for (int num : tangerine)
-    {
-        tangerine_map[num]++;
-    }
-
-    vector<int> counts;
-
-    for(auto& num : tangerine_map)
-    {
-        counts.push_back(num.second);
-    }
-
-    sort(counts.rbegin(), counts.rend());
-
-    int tangerine_count = 0;
-    int size_count = 0;
-    
-    for(int count : counts)
-    {
-        tangerine_count++;
-        size_count += count;
-
-        if(size_count >= k)
-        {
-            answer = tangerine_count;
-            break;
-        }   
-    }
+    int answer = 0;
     return answer;
 }
