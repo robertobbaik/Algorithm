8afa6a7784fd4fd6eb102d584b9b8bce83b1f7fb
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-03-18 12:01:33 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 6.46 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -5,37 +5,50 @@
 
 using namespace std;
 
-int solution(vector<int> queue1, vector<int> queue2) {
-    queue<int> q1, q2;
-    long long sumA = accumulate(queue1.begin(), queue1.end(), 0LL);
-    long long sumB = accumulate(queue2.begin(), queue2.end(), 0LL);
-    long long totalSum = sumA + sumB;
+int solution(vector<int> queue1, vector<int> queue2)
+{
+    int answer = -1;
+    long long q1total = accumulate(queue1.begin(), queue1.end(), 0LL);
+    long long q2total = accumulate(queue2.begin(), queue2.end(), 0LL);
+    long long total = q1total + q2total;
+    long long target = total / 2;
     
-    if (totalSum % 2 != 0) return -1;
-    
-    for (int num : queue1) q1.push(num);
-    for (int num : queue2) q2.push(num);
+    if (total % 2 != 0) return -1;
+    queue<int> q1;
+    queue<int> q2;
 
-    int target = totalSum / 2;
-    int operations = 0;
-    int maxOperations = queue1.size() + queue2.size() * 2; 
-
-    while (sumA != sumB && operations <= maxOperations) {
-        if (sumA > sumB) { 
-            int front = q1.front();
-            q1.pop();
-            q2.push(front);
-            sumA -= front;
-            sumB += front;
-        } else { 
-            int front = q2.front();
-            q2.pop();
-            q1.push(front);
-            sumB -= front;
-            sumA += front;
-        }
-        operations++;
+    for(auto n : queue1)
+    {
+        q1.push(n);
+    }
+    for(auto n : queue2)
+    {
+        q2.push(n);
     }
 
-    return (sumA == sumB) ? operations : -1;
+    int count = 0;
+
+    while(count <= queue1.size() + queue2.size()*2 && q1total != q2total)
+    {
+        if(q1total > q2total)
+        {
+            int num = q1.front();
+            q1.pop();
+            q2.push(num);
+            q1total -= num;
+            q2total += num;
+        }   
+        else
+        {
+            int num = q2.front();
+            q2.pop();
+            q1.push(num);
+            q2total -= num;
+            q1total += num;
+        }   
+        count++;
+    }
+    answer = q1total == q2total ? count : -1;
+
+    return answer;
 }
