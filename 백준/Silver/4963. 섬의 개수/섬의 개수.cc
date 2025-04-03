663686f8d6374fd75028821599c2ed6c221cb41a
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-04-03 09:02:22 +0900

    [Silver II] Title: 섬의 개수, Time: 4 ms, Memory: 2120 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -0,0 +1,71 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int dx[8] = {0, 0, -1, 1, -1, 1, 1, -1};
+int dy[8] = {-1, 1, 0, 0, -1, 1, -1, 1};
+
+void dfs(const vector<vector<int>> &land, vector<vector<bool>> &visited, int x, int y, int w, int h)
+{
+    visited[y][x] = true;
+    //cout << y << " : " << x << endl;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx >= 0 && nx < w && ny >= 0 && ny < h)
+        {
+            if (land[ny][nx] == 1 && !visited[ny][nx])
+            {
+                dfs(land, visited, nx, ny, w, h);
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    //freopen("island.txt", "r", stdin);
+
+    while (true)
+    {
+        int w, h;
+        cin >> w >> h;
+
+        if (w == 0 && h == 0)
+            break;
+
+        vector<vector<int>> land(h, vector<int>(w, 0));
+        vector<vector<bool>> visited(h, vector<bool>(w, false));
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                int n;
+                cin >> n;
+                land[i][j] = n;
+            }
+        }
+        int island_count = 0;
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (land[i][j] == 1 && !visited[i][j])
+                {
+                    dfs(land, visited, j, i, w, h);
+                    island_count++;
+                }
+            }
+        }
+
+        cout << island_count << endl;
+    }
+
+    return 0;
+}
