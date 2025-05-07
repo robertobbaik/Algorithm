93fbaed9c6fb8b584b371b63c2c4b7d81e90a4d7
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-05-07 10:35:46 +0900

    [level 2] Title: 방문 길이, Time: 0.13 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(string dirs)
+{
+    int answer = 0;
+    int x = 5;
+    int y = 5;
+
+    unordered_map<char, pair<int, int>> dir;
+    map<pair<pair<int, int>, pair<int, int>>, bool> m;
+
+    dir['U'] = {0, 1};
+    dir['D'] = {0, -1};
+    dir['L'] = {-1, 0};
+    dir['R'] = {1, 0};
+
+    for (char d : dirs)
+    {
+        auto [dx, dy] = dir[d];
+        int nx = x + dx;
+        int ny = y + dy;
+
+        if (nx < 0 || nx > 10 || ny < 0 || ny > 10)
+            continue;
+
+        if (!m[{{x, y}, {nx, ny}}]) {
+            answer++;
+            m[{{x, y}, {nx, ny}}] = true;
+            m[{{nx, ny}, {x, y}}] = true;
+        }
+
+        x = nx;
+        y = ny;
+    }
+
+    return answer;
+}
