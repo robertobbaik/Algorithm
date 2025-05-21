d3908aca8f8e46515fb6d2ddd7f619fbd44ee0f4
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-05-21 09:41:47 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 320 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+int main(void)
+{
+    //freopen("alien.txt", "r", stdin);
+
+    int N, P;
+
+    cin >> N >> P;
+
+    int count = 0;
+
+    unordered_map<int, priority_queue<int>> um;
+
+    for (int i = 0; i < N; i++)
+    {
+        int line, pret;
+
+        cin >> line >> pret;
+
+        if (um[line].empty())
+        {
+            count++;
+            um[line].push(pret);
+        }
+        else
+        {
+            while (um[line].top() > pret && !um[line].empty())
+            {
+                count++;
+                um[line].pop();
+            }
+
+            if (um[line].top() < pret || um[line].empty())
+            {
+                um[line].push(pret);
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
