2ca238cd67293a070b2b3e26c289e893cdff24b6
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-06-29 17:41:32 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+bool getgroupword(string str)
+{
+    unordered_set<char> um;
+
+    char prev = str[0];
+    for(char c : str)
+    {
+        if(um.find(c) != um.end())
+        {
+            return false;
+        }
+
+        if(c != prev)
+        {
+            um.insert(prev);
+            prev = c;
+        }
+    }
+
+    return true;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("word.txt", "r", stdin);
+
+    int N;
+    cin >> N;
+
+    int result = 0;
+    for (int i = 0; i < N; i++)
+    {
+        string str;
+        cin >> str;
+
+        if(getgroupword(str))
+        {
+            result += 1;
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
