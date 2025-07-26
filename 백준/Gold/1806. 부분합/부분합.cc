325d036a6d2ea3544b193f1a3afecfbe6b4aead1
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-07-26 12:01:32 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,53 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int main(void)
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    //freopen("sequence.txt", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N, M;
+	cin >> N >> M;
 
-    int N, S;
+	vector<int> arr(N);
 
-    cin >> N >> S;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
 
-    vector<int> arr(N);
+	int left = 0;
+	int min_len = N + 1;
+	int sum = 0;
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> arr[i];
-    }
-    int left = 0;
-    int right = 0;
-    int sum = 0;
-
-    int min_len = N + 1;
-
-    while(right < N)
-    {
-        sum += arr[right];
-        while(sum >= S)
-        {
-            min_len = min(min_len, right - left + 1);
-            sum -= arr[left];
-
-            left++;
-        }
-
-        right++;
-    }
+	for (int right = 0; right < N; right++)
+	{
+		sum += arr[right];
+		
+		while(sum >= M)
+		{
+			min_len = min(min_len, right - left + 1);
+			sum -= arr[left];
+			left++;
+		}
+	}
 
     if (min_len == N + 1)
-    {
         cout << 0 << endl;
-    }
     else
-    {
         cout << min_len << endl;
-    }
 
-    return 0;
+	return 0;
 }
