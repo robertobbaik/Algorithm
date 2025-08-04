1001a360c5b54f9d5aeb70eeff19af084da94303
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-08-04 13:21:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> dp(n + 1);
+
+	dp[1] = 1;
+	dp[2] = 3;
+
+	for (int i = 3; i < n + 1; i++)
+	{
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+	}
+
+	cout << dp[n] << endl;
+	return 0;
+}
