58fbba90d33187074f6ef9132483fada38a6bd63
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-08-23 11:24:01 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 948 ms, Memory: 20012 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,71 +1,67 @@
-#include <queue>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-int main(void)
+int main()
 {
-    //freopen("city.txt", "r", stdin);
-    int N, M, K, X;
+	int N, M, K, X;
 
-    cin >> N >> M >> K >> X;
+	cin >> N >> M >> K >> X;
 
-    vector<vector<int>> roads(N + 1);
+	vector<vector<int>> graph(N + 1);
+	queue<pair<int, int>> q;
+	vector<bool> visited(N + 1);
+	vector<int> result;
 
-    for (int i = 0; i < M; i++)
-    {
-        int temp1, temp2;
-        cin >> temp1 >> temp2;
+	for (int i = 0; i < M; i++)
+	{
+		int start, end;
+		cin >> start >> end;
 
-        roads[temp1].push_back(temp2);
-    }
+		graph[start].push_back(end);
+	}
 
-    queue<pair<int, int>> q;
-    vector<int> answer;
-    vector<bool> visited(N + 1, false);
+	q.push({X, 0});
+	visited[X] = true;
+	while (!q.empty())
+	{
+		auto [node, count] = q.front();
 
-    q.push({X, 0});
-    visited[X] = true;
+		q.pop();
 
-    while (!q.empty())
-    {
-        auto [start, distance] = q.front();
+		if (count == K)
+		{
+			result.push_back(node);
 
-        q.pop();
+			continue;
+		}
 
-        if (distance == K)
-        {
-            answer.push_back(start);
-        }
-        else
-        {
-            for (int i = 0; i < roads[start].size(); i++)
-            {
-                int next = roads[start][i];
-                if (!visited[next])
-                {
-                    visited[next] = true;
-                    q.push({next, distance + 1});
-                }
-            }
-        }
-    }
+		for (int next : graph[node])
+		{
+			if (!visited[next])
+			{
+				visited[next] = true;
+				q.push({next, count + 1});
+			}
+		}
+	}
 
-    if (answer.size() == 0)
-    {
-        cout << -1 << endl;
-    }
-    else
-    {
-        sort(answer.begin(), answer.end());
+	if (result.size() > 0)
+	{
+		sort(result.begin(), result.end());
 
-        for (int n : answer)
-        {
-            cout << n << endl;
-        }
-    }
+		for (int n : result)
+		{
+			cout << n << "\n";
+		}
+	}
+	else
+	{
+		cout << -1 << endl;
+	}
 
-    return 0;
+	return 0;
 }
