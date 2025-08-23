b8aa43822602798b6f0c50df51b7634166f32c57
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-08-23 16:12:07 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,74 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include <queue>
 
 using namespace std;
 
-struct Node
-{
-    int x;
-    int y;
-    int distance;
-};
-
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
 
-int main(void)
+int main()
 {
-    //freopen("miro.txt", "r", stdin);
+	int N, M;
+	cin >> N >> M;
 
-    int N, M;
+	vector<vector<int>> board(N, vector<int>(M, 0));
+	vector<vector<bool>> visited(N, vector<bool>(M, false));
 
-    cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		string str;
+		cin >> str;
+		for (int j = 0; j < M; j++)
+		{
+			board[i][j] = str[j] - '0';
+		}
+	}
+	
+	queue<tuple<int, int, int>> q;
+	q.push({0, 0, 1});
+	visited[0][0] = true;
 
-    vector<vector<int>> board(N, vector<int>(M, 0));
-    vector<vector<bool>> visited(N, vector<bool>(M, false));
+	while (!q.empty())
+	{
+		auto [x, y, count] = q.front();
+		q.pop();
 
-    for (int i = 0; i < N; i++)
-    {
-        string temp;
-        cin >> temp;
-        for (int j = 0; j < M; j++)
-        {
-            board[i][j] = temp[j] - '0';
-        }
-    }
+		if (x == N - 1 && y == M - 1)
+		{
+			cout << count << endl;
+			return 0;
+		}
 
-    queue<Node> q;
-    q.push({0, 0, 1});
-    visited[0][0] = true;
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 
-    while (!q.empty())
-    {
-        Node current = q.front();
-        int x = current.x;
-        int y = current.y;
+			if (nx >= 0 && nx < N && ny >= 0 && ny < M)
+			{
+				if (!visited[nx][ny] && board[nx][ny] == 1)
+				{
+					visited[nx][ny] = true;
+					q.push({nx, ny, count + 1});
+				}
+			}
+		}
+	}
 
-        q.pop();
-
-        if (x == M - 1 && y == N - 1)
-        {
-            cout << current.distance << endl;
-            break;
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && nx < M && ny >= 0 && ny < N)
-            {
-                if (board[ny][nx] == 1 && !visited[ny][nx])
-                {
-                    // cout << nx << ", " << ny << endl;
-                    visited[ny][nx] = true;
-                    q.push({nx, ny, current.distance + 1});
-                }
-            }
-        }
-    }
-
-    return 0;
+	return 0;
 }
