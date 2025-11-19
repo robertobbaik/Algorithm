e25adcde196ec4a894e5f7f4fe03683c98c180d4
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2025-11-19 14:29:56 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,61 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int N, M;
-vector<vector<char>> board;
-
-int getwrongcount(int startN, int startM, char startColor)
+int check(const vector<vector<char>> &board, int x, int y, char startColor)
 {
-    int wrong_count = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            char expectedColor = ((i + j) % 2 == 0) ? startColor : (startColor == 'W' ? 'B' : 'W');
-            if (board[startN + i][startM + j] != expectedColor)
-            {
-                wrong_count++;
-            }
-        }
-    }
-    return wrong_count;
+	int wrongCount = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			char color = ((i + j) % 2 == 0) ? startColor : (startColor == 'W' ? 'B' : 'W');
+			if (board[x + i][y + j] != color)
+			{
+				wrongCount++;
+			}
+		}
+	}
+	return wrongCount;
 }
 
-int main(void)
+int main()
 {
-   // freopen("chess.txt", "r", stdin);
+	int N, M;
+	cin >> N >> M;
 
-    cin >> N >> M;
+	vector<vector<char>> board(N, vector<char>(M));
 
-    board.resize(N, vector<char>(M));
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> board[i][j];
+		}
+	}
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cin >> board[i][j];
-        }
-    }
+	int startX = N - 7;
+	int startY = M - 7;
+	int minCount = 64;
 
-    int splitN = N - 8;
-    int splitM = M - 8;
+	for (int i = 0; i < startX; i++)
+	{
+		for (int j = 0; j < startY; j++)
+		{
+			int wcount = check(board, i, j, 'W');
+			int bcount = check(board, i, j, 'B');
 
-    // 15 16 17 18 19 20 21 22 23
-    
-    int min_count = 64;
-    for (int i = 0; i <= splitN; i++)
-    {
-        for (int j = 0; j <= splitM; j++)
-        {
-            int wcount = getwrongcount(i, j, 'W');
-            int bcount = getwrongcount(i, j, 'B');
-            
-            min_count = min({min_count, wcount, bcount});
-        }
-    }
-    cout << min_count << endl;
+			minCount = min({wcount, bcount, minCount});
+		}
+	}
 
-    return 0;
+	cout << minCount << endl;
+	return 0;
 }
