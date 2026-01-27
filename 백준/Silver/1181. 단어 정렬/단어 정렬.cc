0ec0be5374d610f1956c41fa3624ec0b96fb742e
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-01-27 09:28:23 +0900

    [Silver V] Title: 단어 정렬, Time: 24 ms, Memory: 7320 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,48 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include <unordered_set>
 
 using namespace std;
 
-bool compare(const string &a, const string &b)
+bool compare(string &a, string &b)
 {
-    if (a.size() == b.size())
-    {
-        return a < b;
-    }
+	if (a.size() == b.size())
+	{
+		return a < b;
+	}
 
-    return a.size() < b.size();
+	return a.size() < b.size();
 }
 
-int main(void)
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    //freopen("sort.txt", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int N;
-    cin >> N;
-    unordered_set<string> s;
-    vector<string> arr;
-    for (int i = 0; i < N; i++)
-    {
-        string str;
-        cin >> str;
-        s.insert(str);
-    }
+	int N;
+	cin >> N;
 
-    for (string str : s)
-    {
-        arr.push_back(str);
-    }
+	vector<string> arr;
+	unordered_set<string> us;
 
-    sort(arr.begin(), arr.end(), compare);
+	for (int i = 0; i < N; i++)
+	{
+		string str;
+		cin >> str;
+		us.insert(str);
+	}
 
-    for (string str : arr)
-    {
-        cout << str << "\n";
-    }
+	for (string str : us)
+	{
+		arr.push_back(str);
+	}
 
-    return 0;
+	sort(arr.begin(), arr.end(), compare);
+
+	for (string str : arr)
+	{
+		cout << str << "\n";
+	}
+
+	return 0;
 }
