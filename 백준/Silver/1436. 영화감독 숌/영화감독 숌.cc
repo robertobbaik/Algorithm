24df09f51629000af55a70dd85531f219994c73d
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-01-27 10:16:22 +0900

    [Silver V] Title: 영화감독 숌, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,28 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <string>
-
 using namespace std;
 
-int main(void)
+int main()
 {
-    int N;
+	int N;
+	cin >> N;
 
-    cin >> N;
+	int count = 1;
+	int start = 666;
 
-    int count = 1;
-    int numbering = 666;
+	while(count < N)
+	{
+		start++;
+		if(to_string(start).find("666") != string::npos)
+		{
+			count++;
+		}
+	}
 
-    while (count < N)
-    {
-        numbering++;
+	cout << start << endl;
 
-        if (to_string(numbering).find("666") != string::npos)
-        {
-            count++;
-        }
-    }
-
-    cout << numbering << endl;
-    return 0;
+	return 0;
 }
