825b3026f83447ae35109bcf271e398f450193ca
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-02-13 10:28:13 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,37 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(void)
+int main()
 {
-    //freopen("coin.txt", "r", stdin);
-    int N, K;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    cin >> N >> K;
+	int N, K;
 
-    vector<int> coins(N);
+	cin >> N >> K;
 
-    for(int i = 0; i < N; i++)
-    {
-        cin >> coins[i];
-    }
+	vector<int> coins(N);
 
-    int target = 0;
-    int count = 0;
-    while(target < K)
-    {
-        for(int i = N - 1; i >= 0; i--)
-        {
-            if(target + coins[i] <= K)
-            {
-                target += coins[i];
-                count++;
-                break;
-            }
-        }
-    }
-    cout << count << endl;
+	for(int i = 0; i < N; i++)
+	{
+		cin >> coins[i];
+	}
 
-    return 0;
+	sort(coins.rbegin(), coins.rend());
+
+	int coincount = 0;
+
+	for(int i = 0; i < N; i++)
+	{
+		if(K == 0) break;
+
+		coincount += (K / coins[i]);
+		K %= coins[i];
+	}
+ 
+
+	cout << coincount << "\n";
+
+
+	return 0;
 }
