e96aad5bc4882d8f0d22fb8c9f0f2d1973be78c2
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-02-23 09:44:23 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,14 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int A, B;
-    cin >> A >> B;
-    
-    cout << A + B << endl;
-    return 0;
+int main()
+{
+	int A, B;
+
+	cin >> A >> B;
+
+	cout << A + B << endl;
+	return 0;
 }
