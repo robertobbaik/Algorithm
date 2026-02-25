3acfd96d465e6940d3a2a2713a8d4e11fa74b749
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-02-25 13:20:00 +0900

    [level 2] Title: 올바른 괄호, Time: 0.52 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 3.82 MB, 시간: 0.64 ms
+메모리: 3.88 MB, 시간: 0.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 17일 10:38:18
+2026년 02월 25일 13:19:58
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,14 +1,42 @@
-#include<string>
-#include <iostream>
+// 프로그래머스 - 올바른 괄호
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+#include <string>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
 bool solution(string s)
 {
     bool answer = true;
+    
+    stack<char> st;
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
+    for(char c : s)
+    {
+        if(c == '(')
+        {
+            st.push(c);
+        }
+        else
+        {
+            if(st.empty())
+            {
+                answer = false;
+                break;
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+    }
 
+    if(!st.empty())
+    {
+        answer = false;
+    }
+    
     return answer;
 }
