698a1541ddf15c7b357c7267df7cf0c8b92503f1
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-02-27 10:27:37 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,19 +1,29 @@
+// 프로그래머스 - H-Index
+// https://school.programmers.co.kr/learn/courses/30/lessons/42747
+
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> citations) {
-    sort(citations.begin(), citations.end(), greater<int>());
-    
-    int h_index = 0;
-    for (int i = 0; i < citations.size(); i++) {
-        if (citations[i] >= i + 1) {
-            h_index = i + 1;
-        } else {
+int solution(vector<int> citations)
+{
+    int answer = 0;
+
+    sort(citations.rbegin(), citations.rend()); // [6,5,3,1,0]
+
+    for (int i = 0; i < citations.size(); ++i)
+    {
+        if (citations[i] >= i + 1)
+        {
+            answer = i + 1;
+        }
+        else
+        {
             break;
         }
     }
-    
-    return h_index;
+
+    return answer;
 }
