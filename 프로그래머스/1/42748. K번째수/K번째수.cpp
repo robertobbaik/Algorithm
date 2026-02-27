5cff9c25fcf811f64146c71bd8675ee1eaceaa7a
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-02-27 10:00:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,44 +1,34 @@
+// 프로그래머스 - K번째수
+// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void bubblesort(vector<int>& a)
+vector<int> solution(vector<int> array, vector<vector<int>> commands)
 {
-    for(int i = 0; i < a.size(); i++)
-    {
-        for(int j = i + 1; j < a.size(); j++)
-        {
-            if(a[i] > a[j])
-            {
-                swap(a[i], a[j]);
-            }
-        }
-    }
-}
-
-void swap(int& a, int& b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    
-    for(int i = 0; i < commands.size(); i++)
+
+    int num = commands.size();
+
+    for(int i = 0; i < num; i++)
     {
-        vector<int> temp;
+        int start = commands[i][0];
+        int end = commands[i][1];
+        int target = commands[i][2];
         
-        for(int j = commands[i][0] - 1; j < commands[i][1]; j++)
-        { 
-            temp.push_back(array[j]);
+        vector<int> arr;
+        for(int j = start - 1; j < end; j++)
+        {
+            arr.push_back(array[j]);
         }
-        
-        bubblesort(temp);
-        answer.push_back(temp[commands[i][2] - 1]);
+
+        sort(arr.begin(), arr.end());
+
+        answer.push_back(arr[target - 1]);
+
     }
-    
     return answer;
 }
