4a3ec8fa3177af59ff5a0a2d82467e4f0bc25678
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-01 00:13:20 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,29 +1,28 @@
+// BOJ #2231 - 분해합
+// https://www.acmicpc.net/problem/2231
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-int main(void)
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    //freopen("digitgenerator.txt", "r", stdin);
 
     int N;
+
     cin >> N;
 
     for (int i = 1; i < N; i++)
     {
         int temp = i;
         int sum = 0;
-
         while (temp)
         {
             sum += temp % 10;
             temp /= 10;
         }
 
-        if (i + sum == N)
+        if (sum + i == N)
         {
             cout << i << endl;
             return 0;
