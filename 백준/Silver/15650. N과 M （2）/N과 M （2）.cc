d3669d50b7d13d7fcff1795c335036d01edc3e8c
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-18 10:57:49 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,38 +1,47 @@
+// BOJ #15650 - N과 M (2)
+// https://www.acmicpc.net/problem/15650
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-void backtracking(vector<int> &arr, int depth, int start, int N, int M)
-{
-    if (depth == M)
-    {
-        for (int i = 0; i < M; i++)
-        {
-            cout << arr[i] << " ";
-        }
+// TODO
 
-        cout << "\n";
+//중복없이, 오름차순
+
+void backtracking(int N, int M, vector<int>& arr, int start, int depth)
+{
+    if(depth == M)
+    {
+        for(int i = 0; i < M; i++)
+        {
+            cout << arr[i] << " \n"[i == M - 1];
+        }
         return;
     }
 
-    for (int i = start; i <= N; i++)
+    for(int i = start; i <= N; i++)
     {
-        arr[depth] = i;
-        backtracking(arr, depth + 1, i + 1, N, M);
+        if(i > start)
+        {
+            arr[depth] = i;
+            start++;
+            backtracking(N, M, arr, start, depth + 1);
+            
+        }
     }
 }
 
-int main(void)
+int main()
 {
-    //freopen("NM1.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int N, M;
-
     cin >> N >> M;
 
-    vector<int> arr(M);
-    backtracking(arr, 0, 1, N, M);
+    // TODO
+    vector<int> arr(8, 0);
+    backtracking(N, M, arr, 0, 0);
 
     return 0;
 }
