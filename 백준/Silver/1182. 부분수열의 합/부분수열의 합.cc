ca3b0bd5ab037c025589f646ee224212077f70c7
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-19 11:16:07 +0900

    [Silver II] Title: 부분수열의 합, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,45 +1,49 @@
+// BOJ #1182 - 부분수열의 합
+// https://www.acmicpc.net/problem/1182
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int N, S;
+    int N, S;
+    cin >> N >> S;
 
-	cin >> N >> S;
+    vector<int> arr(N);
 
-	vector<int> arr(N);
+    for(int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
 
-	for (int i = 0; i < N; i++)
-	{
-		cin >> arr[i];
-	}
-	int count = 0;
+    int answer = 0;
 
-	for (int mask = 1; mask < (1 << N); mask++)
-	{
-		int sum = 0;
-		for (int i = 0; i < N; i++)
-		{
-			if (mask & (1 << i))
-			{
-				sum += arr[i];
-			}
-		}
+    for(int i = 1; i < (1 << N); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < N; j++)
+        {
+            if(i & (1 << j))
+            {
+                sum += arr[j];
+            }
+        }
 
-		if (sum == S)
-		{
-			count++;
-		}
-		
-	}
+        if(sum == S)
+        {
+            answer++;
+        }
+    }
 
-	cout << count << endl;
+    cout << answer << endl;
 
-	return 0;
+    return 0;
 }
+
+// [코드 평가]
+// 정확한 풀이. i=1부터 시작해 공집합 제외, 비트마스크로 2^N 부분집합 순회
