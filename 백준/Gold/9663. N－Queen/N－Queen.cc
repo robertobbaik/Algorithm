c4615ba08c429da7f05945cc78febe9e0a552f7c
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-19 10:41:27 +0900

    [Gold IV] Title: N-Queen, Time: 7296 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,35 +1,42 @@
+// BOJ #9663 - N-Queen
+// https://www.acmicpc.net/problem/9663
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int N, total;
-vector<vector<bool>> board;
+int total;
 
-bool isValid(int row, int col)
+bool isValid(int N, vector<vector<bool>> &board, int row, int col)
 {
     for (int i = 0; i < row; i++)
     {
         if (board[i][col])
+        {
             return false;
+        }
     }
 
     for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
     {
         if (board[i][j])
+        {
             return false;
+        }
     }
 
     for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++)
     {
         if (board[i][j])
+        {
             return false;
+        }
     }
 
     return true;
 }
 
-void nqueen(int row)
+void backtracking(int N, int row, vector<vector<bool>> &board)
 {
     if (row == N)
     {
@@ -39,27 +46,31 @@ void nqueen(int row)
 
     for (int i = 0; i < N; i++)
     {
-        if (isValid(row, i))
+        if (isValid(N, board, row, i))
         {
             board[row][i] = true;
-            nqueen(row + 1);
+            backtracking(N, row + 1, board);
             board[row][i] = false;
         }
     }
 }
 
-int main(void)
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    //freopen("N-Queen.txt", "r", stdin);
 
+    int N;
     cin >> N;
 
-    board.resize(N, vector<bool>(N, false));
+    vector<vector<bool>> board(N, vector<bool>(N, false));
 
-    nqueen(0);
+    backtracking(N, 0, board);
     cout << total << endl;
 
     return 0;
 }
+
+// [코드 평가]
+// 정확한 풀이. 행 단위로 퀸을 놓고, isValid에서 열/대각선 충돌을 모두 검사함
+// 선택-재귀-취소 패턴도 올바르게 적용됨
