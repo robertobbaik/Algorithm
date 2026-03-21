80af5ebdc5d3a71058f638e3f3cb2f9e2cc1a741
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-21 10:52:27 +0900

    [level 2] Title: 피로도, Time: 8.51 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,57 +1,41 @@
+// 프로그래머스 - 피로도
+// https://school.programmers.co.kr/learn/courses/30/lessons/87946
+
 #include <string>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
+int answer = -1;
+
+void backtracking(int k, vector<vector<int>>& dungeons, vector<bool>& visited, int depth)
+{
+    for(int i = 0; i < dungeons.size(); i++)
+    {
+        if(!visited[i] && dungeons[i][0] <= k)
+        {
+            visited[i] = true;
+            backtracking(k - dungeons[i][1], dungeons, visited, depth + 1);
+            visited[i] = false;
+        }
+    }
+
+    answer = max(depth, answer);
+}
+
 int solution(int k, vector<vector<int>> dungeons)
 {
-    int answer = -1;
+    vector<bool> visited(dungeons.size(), false);
 
-    vector<vector<int>> v;
-
-    vector<int> temp;
-    int max_count = 0;
-
-    for (size_t i = 0; i < dungeons.size(); i++)
+    for(int i = 0; i < dungeons.size(); i++)
     {
-        temp.push_back(i);
+        backtracking(k, dungeons, visited, 0);
     }
 
-    do
-    {
-        vector<int> a;
 
-        for (int num : temp)
-        {
-            a.push_back(num);
-        }
-
-        v.push_back(a);
-        a.clear();
-
-    } while (next_permutation(temp.begin(), temp.end()));
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        int count = 0;
-        int energy = k;
-        for (int j = 0; j < v[i].size(); ++j)
-        {
-            if(dungeons[v[i][j]][0] <= energy && energy - dungeons[v[i][j]][1] >= 0)
-            {
-                energy -= dungeons[v[i][j]][1];
-                ++count;
-            } 
-        }
-
-        if(max_count < count)
-        {
-            max_count = count;
-        }
-    }
-
-    answer = max_count;
+    
 
     return answer;
 }
