0938466e16bd52f611ae32313000fc23f1fbaaae
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-23 16:20:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 2361.88 ms, Memory: 5.33 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,41 +1,34 @@
-// 프로그래머스 - 큰 수 만들기
-// https://school.programmers.co.kr/learn/courses/30/lessons/42883
-
 #include <string>
 #include <vector>
 #include <stack>
 
 using namespace std;
-
-
 string solution(string number, int k)
 {
     string answer = "";
-    int len = number.size();
     stack<char> s;
+
+    int count = k;
 
     for(char c : number)
     {
-        while(!s.empty() && s.top() < c && k > 0)
+        while(!s.empty() && s.top() < c && count > 0)
         {
             s.pop();
-            k--;
+            count--;
         }
         s.push(c);
     }
 
-    while(k > 0)
+    while(count > 0)
     {
         s.pop();
-        k--;
+        count--;
     }
 
-    while(!s.empty())
-    {
+    while (!s.empty()) {
         answer = s.top() + answer;
         s.pop();
     }
-    
-
     return answer;
 }
