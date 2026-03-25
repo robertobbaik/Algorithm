28e49cf5c655aee2439a41bdd81cb895023a3c5f
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-03-25 09:31:47 +0900

    [Silver I] Title: 안전 영역, Time: 24 ms, Memory: 2492 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,72 +1,79 @@
+// BOJ #2468 - 안전 영역
+// https://www.acmicpc.net/problem/2468
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int N;
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {-1, 1, 0, 0};
 
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-
-void dfs(const vector<vector<int>> &graph, vector<vector<bool>> &visited, int x, int y, int height)
+void dfs(vector<vector<int>> &board, vector<vector<bool>> &visited, int N, int limit, int x, int y)
 {
-	visited[x][y] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
 
-	for (int i = 0; i < 4; i++)
-	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		if (nx >= 0 && nx < N && ny >= 0 && ny < N)
-		{
-			if (!visited[nx][ny] && graph[nx][ny] > height)
-			{
-				dfs(graph, visited, nx, ny, height);
-			}
-		}
-	}
+        if (nx >= 0 && nx < N && ny >= 0 && ny < N)
+        {
+            if (!visited[nx][ny] && board[nx][ny] > limit)
+            {
+                visited[nx][ny] = true;
+                dfs(board, visited, N, limit, nx, ny);
+            }
+        }
+    }
 }
 
 int main()
 {
-	cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	vector<vector<int>> graph(N, vector<int>(N, 0));
-	vector<vector<bool>> visited(N, vector<bool>(N, false));
+    int N;
+    cin >> N;
 
-	int max_height = 0;
+    vector<vector<int>> board(N, vector<int>(N, 0));
 
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cin >> graph[i][j];
-			max_height = max(max_height, graph[i][j]);
-		}
-	}
+    int max_num = 0;
+    int min_num = 101;
+    int answer = 1;
 
-	int maxsafeareas = 1;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int num;
+            cin >> num;
+            board[i][j] = num;
 
-	for (int k = 1; k < max_height; k++)
-	{
-		visited.assign(N, vector<bool>(N, false));
-		int count = 0;
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < N; j++)
-			{
-				if (!visited[i][j] && graph[i][j] > k)
-				{
-					count++;
-					dfs(graph, visited, i, j, k);
-				}
-			}
-		}
-		maxsafeareas = max(maxsafeareas, count);
-	}
+            max_num = max(num, max_num);
+            min_num = min(num, min_num);
+        }
+    }
 
-	cout << maxsafeareas << endl;
+    for (int i = min_num; i <= max_num; i++)
+    {
+        vector<vector<bool>> visited(N, vector<bool>(N, false));
+        int area = 0;
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                if (!visited[j][k] && board[j][k] > i)
+                {
+                    area++;
+                    visited[j][k] = true;
+                    dfs(board, visited, N, i, j, k);
+                    answer = max(area, answer);
+                }
+            }
+        }
+    }
 
-	return 0;
+    cout << answer << endl;
+
+    return 0;
 }
