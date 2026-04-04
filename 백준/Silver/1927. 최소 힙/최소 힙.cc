98f387b148722c91fbc6adbdee14389c77c2b8ff
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-04-04 18:57:14 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,20 +1,18 @@
+// BOJ #1927 - 최소 힙
+// https://www.acmicpc.net/problem/1927
 #include <iostream>
-#include <vector>
 #include <queue>
-
 using namespace std;
 
-int main(void)
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //freopen("minheap.txt", "r", stdin);
-    priority_queue<int, vector<int>, greater<int>> pq;
-
     int N;
-
     cin >> N;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
 
     for(int i = 0; i < N; i++)
     {
@@ -23,14 +21,14 @@ int main(void)
 
         if(num == 0)
         {
-            if(pq.empty())
+            if(!pq.empty())
             {
-                cout << 0 << "\n";
+                cout << pq.top() << '\n';
+                pq.pop();
             }
             else
             {
-                cout << pq.top() << "\n";
-                pq.pop();
+                cout << 0 << '\n';
             }
         }
         else
