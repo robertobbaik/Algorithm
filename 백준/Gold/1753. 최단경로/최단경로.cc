4f13990d8319b90316222f4fb8e7e4cb18320586
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-04-07 11:21:08 +0900

    [Gold IV] Title: 최단경로, Time: 96 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,3 +1,5 @@
+// BOJ #1753 - 최단경로
+// https://www.acmicpc.net/problem/1753
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -5,30 +7,27 @@
 
 using namespace std;
 
-int main(void)
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    //freopen("1753.txt", "r", stdin);
 
-    int V, E;
+    int V, E, start;
 
-    cin >> V >> E;
-
-    int start;
-    cin >> start;
+    cin >> V >> E >> start;
 
     vector<vector<pair<int, int>>> graph(V + 1);
+    vector<int> distance(V + 1, INT_MAX);
 
     for (int i = 0; i < E; i++)
     {
         int u, v, w;
+
         cin >> u >> v >> w;
 
         graph[u].push_back({v, w});
     }
 
-    vector<int> distance(V + 1, INT_MAX);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
     pq.push({0, start});
@@ -40,23 +39,25 @@ int main(void)
         pq.pop();
 
         if (distance[currentNode] < currentDistance)
+        {
             continue;
+        }
 
-        for (auto [neigborNode, cost] : graph[currentNode])
+        for (auto [neighborNode, cost] : graph[currentNode])
         {
             int newDistance = distance[currentNode] + cost;
 
-            if (newDistance < distance[neigborNode])
+            if (newDistance < distance[neighborNode])
             {
-                distance[neigborNode] = newDistance;
-                pq.push({newDistance, neigborNode});
+                distance[neighborNode] = newDistance;
+                pq.push({newDistance, neighborNode});
             }
         }
     }
-    
-    for(int i = 1; i < V + 1; i++)
+
+    for (int i = 1; i < V + 1; i++)
     {
-        if(distance[i] == INT_MAX)
+        if (distance[i] == INT_MAX)
         {
             cout << "INF" << "\n";
         }
