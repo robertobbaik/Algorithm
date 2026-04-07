1d7519c1b0b6b9878f9474bf5ec7ea6e59f20e5e
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-04-07 11:55:43 +0900

    [Gold III] Title: 파티, Time: 100 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,70 +1,80 @@
+// BOJ #1238 - 파티
+// https://www.acmicpc.net/problem/1238
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <climits>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-int dijkstra(const vector<vector<pair<int, int>>> &graph, int start, int end, int N)
-{
-    vector<int> distance(N + 1, INT_MAX);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    distance[start] = 0;
-    pq.push({0, start});
-
-    while (!pq.empty())
-    {
-        auto [currentDistance, currentNode] = pq.top();
-        pq.pop();
-
-        if (distance[currentNode] < currentDistance)
-            continue;
-
-        for (auto [neigborNode, cost] : graph[currentNode])
-        {
-            int newDistance = currentDistance + cost;
-            if (newDistance < distance[neigborNode])
-            {
-                distance[neigborNode] = newDistance;
-                pq.push({newDistance, neigborNode});
-            }
-        }
-    }
-
-    return distance[end];
-}
-
-int main(void)
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    //freopen("1238.txt", "r", stdin);
 
     int N, M, X;
+
     cin >> N >> M >> X;
 
     vector<vector<pair<int, int>>> graph(N + 1);
 
     for (int i = 0; i < M; i++)
     {
-        int s, e, c;
-        cin >> s >> e >> c;
-        graph[s].push_back({e, c});
+        int u, v, w;
+
+        cin >> u >> v >> w;
+
+        graph[u].push_back({v, w});
     }
 
-    vector<int> distance(N + 1);
+    vector<int> returnDistace(N + 1, 0);
 
-    for (int i = 1; i <= N; i++)
+    for (int i = 1; i < N + 1; i++)
     {
-        int dis = dijkstra(graph, i, X, N) + dijkstra(graph, X, i, N);
-        distance[i] = dis;
+        vector<int> distance(N + 1, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        distance[i] = 0;
+        pq.push({0, i});
+
+        while (!pq.empty())
+        {
+            auto [currentDistance, currentNode] = pq.top();
+            pq.pop();
+
+            if (distance[currentNode] < currentDistance)
+            {
+                continue;
+            }
+
+            for (auto [neighborNode, cost] : graph[currentNode])
+            {
+                int newDistance = distance[currentNode] + cost;
+
+                if (newDistance < distance[neighborNode])
+                {
+                    distance[neighborNode] = newDistance;
+                    pq.push({newDistance, neighborNode});
+                }
+            }
+        }
+
+
+        if (i == X)
+        {
+            for (int j = 1; j < N + 1; j++)
+            {
+                returnDistace[j] += distance[j];
+            }
+        }
+        else
+        {
+            returnDistace[i] += distance[X];
+        }
     }
 
-    int max = *max_element(distance.begin(), distance.end());
-
-    cout << max << endl;
+    cout << *max_element(returnDistace.begin() + 1, returnDistace.end()) << endl;
 
     return 0;
 }
