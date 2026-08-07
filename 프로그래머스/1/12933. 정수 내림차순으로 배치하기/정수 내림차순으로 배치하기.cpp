53483db7c47e9321a8c9ac9c2defc6f496f4971d
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-07 12:00:10 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,39 +1,19 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-long long solution(long long n) {
+long long solution(long long n)
+{
     long long answer = 0;
-    vector<long long> temp;
-    
-    while(n)
-    {
-        temp.push_back(n % 10);    
-        n /= 10;
-    }
 
-    for(int i = 0; i < temp.size(); i++)
-    {
-        bool isSwaped = false;
-        
-        for(int j = i + 1; j < temp.size(); j++)
-        {
-            if(temp[i] < temp[j])
-            {
-                int a = temp[i];
-                temp[i] = temp[j];
-                temp[j] = a;
-            }
-        }
-    }
-    
-    int b = 1;
-    for(int i = temp.size() - 1; i >= 0; i--)
-    {
-        answer += temp[i] * b;
-        b *= 10;
-    }
-    
+    string str = to_string(n);
+
+    sort(str.rbegin(), str.rend());
+
+    answer = stoll(str);
+
     return answer;
 }
