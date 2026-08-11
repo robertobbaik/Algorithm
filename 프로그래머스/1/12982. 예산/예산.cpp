676d129457c7bfaffec21aff069116be739bd780
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-11 09:47:57 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 4.88 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget)
+{
+    int answer = 0;
+
+    sort(d.begin(), d.end());
+
+    for(int i = 0; i < d.size(); i++)
+    {
+        if(d[i] <= budget)
+        {
+            budget -= d[i];
+            answer++;
+        }
+    }
+
+    return answer;
+}
