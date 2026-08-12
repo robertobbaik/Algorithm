89696e0bdf63aa9a7847c159c8d27fcb23fdb5dc
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-12 10:04:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,27 +1,29 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-string solution(vector<int> food) {
+string solution(vector<int> food)
+{
     string answer = "";
-    
-    vector<int> temp;
+    stack<char> st;
+
     for(int i = 1; i < food.size(); i++)
-    {    
-       for(int j = 0; j < food[i] / 2; j++)
-       {
-            answer += to_string(i);     
-       }
-    }
-    answer += "0";
-    
-    int len = answer.size();
-    
-    for(int i = len - 2; i >= 0; i--)
     {
-        answer.push_back(answer[i]);
+        for(int j = 0; j < food[i] / 2; j++)
+        {
+            answer += i + '0';
+            st.push(i + '0');
+        }
     }
-    
+
+    answer += '0';
+
+    while(!st.empty())
+    {
+        answer += st.top();
+        st.pop();
+    }
     return answer;
 }
