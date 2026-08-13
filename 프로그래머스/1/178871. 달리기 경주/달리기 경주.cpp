da94a7f8c4341ca673bf5f551947e1b9ae657abe
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-13 10:35:05 +0900

    [level 1] Title: 달리기 경주, Time: 227.89 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -4,35 +4,29 @@
 
 using namespace std;
 
-vector<string> solution(vector<string> players, vector<string> callings) {
+vector<string> solution(vector<string> players, vector<string> callings)
+{
     vector<string> answer;
-    
-        unordered_map<string, int> tempMap;
-    unordered_map<int, string> tempMap2;
-    
-      for (int i = 0; i < players.size(); ++i)
+
+    unordered_map<string, int> um;
+
+    for(int i = 0; i < players.size(); i++)
     {
-        tempMap[players[i]] = i;
-        tempMap2[i] = players[i];
+        um[players[i]] = i;
     }
 
-
-    for (int i = 0; i < callings.size(); ++i)
+    for(string str : callings)
     {
-        int cur = tempMap[callings[i]];
-        tempMap[callings[i]] -= 1;
-        string temp = tempMap2[cur - 1]; 
-        tempMap[temp] = cur;
-        
-        tempMap2[cur] = temp;
-        tempMap2[cur - 1] = callings[i];
+        int idx = um[str];
+
+        string temp = players[idx - 1];
+        players[idx - 1] = str;
+        players[idx] = temp;
+
+        um[str]--;
+        um[temp]++;
     }
 
-    for (int i = 0; i < tempMap.size(); ++i)
-    {
-        answer.push_back(tempMap2[i]);
-    }
-    
-    
+    answer = players;
     return answer;
 }
