2c3cd8fed499a080005a0da172ef416931d0db9e
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-14 09:51:04 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int a, int b)
+{
+    int answer = max(stoi(to_string(a) + to_string(b)), stoi(to_string(b)+ to_string(a)));
+
+
+    return answer;
+}
