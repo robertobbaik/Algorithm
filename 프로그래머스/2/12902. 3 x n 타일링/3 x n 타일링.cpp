b00c316720bb1c82cb335f8ebfb391ff1578da97
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-15 10:18:17 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.03 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -7,14 +7,14 @@ int solution(int n)
 {
     int answer = 0;
 
-    vector<long long> dp(n + 1);
+    vector<int> dp(n + 1);
 
     dp[2] = 3;
     dp[4] = 11;
 
     for(int i = 6; i < n + 1; i += 2)
     {
-        dp[i] = (4 * dp[i - 2] - dp[i - 4] + 1000000007) % 1000000007;
+        dp[i] = (dp[i - 4] + dp[i - 2]) % 1000000007;
     }
 
     answer = dp[n];
