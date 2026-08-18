9416ee9ecacaa817da46434aadf889768189b235
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-18 09:59:30 +0900

    [level 2] Title: 숫자의 표현, Time: 0.01 ms, Memory: 4.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+
+    int left = 1;
+    int sum = 0;
+
+    for (int right = 1; right < n + 1; right++)
+    {
+        sum += right;
+        
+        while(sum > n)
+        {
+            sum -= left;
+            left++;
+        }
+
+        if(sum == n)
+        {
+            answer++;
+        }
+    }
+
+    return answer;
+}
