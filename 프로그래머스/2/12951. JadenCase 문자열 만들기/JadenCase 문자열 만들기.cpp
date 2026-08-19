04bc9b72d451f6619865d284ab91f74054763d20
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-19 10:39:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -3,39 +3,23 @@
 
 using namespace std;
 
-string solution(string s) {
-   string answer = "";
-    bool isUppercase = true;
+string solution(string s)
+{
+    string answer = "";
 
-    for (int i = 0; i < s.size(); ++i)
+    answer += toupper(s[0]);
+
+    for(int i = 1; i < s.size(); i++)
     {
-        if (s[i] == ' ')
+        if(s[i - 1] == ' ')
         {
-            isUppercase = true;
+            answer += toupper(s[i]);
         }
-        else if ((int)s[i] < 65)
+        else
         {
-            isUppercase = false;
-        }
-        else if ((int)s[i] >= 65 && (int)s[i] < 97)
-        {
-            if(!isUppercase)
-            {
-                s[i] = (char)((int)s[i] + 32);
-            }
-            else
-            {
-                isUppercase = false;
-            }
-            
-        }
-        else if ((int)s[i] > 96 && isUppercase)
-        {
-            s[i] = (char)((int)s[i] - 32);
-            isUppercase = false;
+            answer += tolower(s[i]);
         }
     }
-    answer = s;
 
     return answer;
 }
