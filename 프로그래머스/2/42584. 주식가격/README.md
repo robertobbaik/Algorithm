e598108701dae6ea747720998d7017aeaeebd422
robertobbaik <166184601+robertobbaik@users.noreply.github.com>
2026-08-27 11:00:40 +0900

    [level 2] Title: 주식가격, Time: 23.57 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 26.8 MB, 시간: 27.72 ms
+메모리: 27 MB, 시간: 23.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 26일 11:27:21
+2026년 08월 27일 11:00:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,30 +1,26 @@
-// 프로그래머스 - 주식가격
-// https://school.programmers.co.kr/learn/courses/30/lessons/42584
-
 #include <string>
 #include <vector>
+
 
 using namespace std;
 
 vector<int> solution(vector<int> prices)
 {
-    vector<int> answer;
+    vector<int> answer(prices.size(), 0);
 
     for(int i = 0; i < prices.size(); i++)
     {
-        int n = prices[i];
-        int seconds = 0;
         for(int j = i + 1; j < prices.size(); j++)
         {
-            seconds++;
-            if(n > prices[j])
+            answer[i]++;
+
+            if(prices[j] < prices[i])
             {
                 break;
             }
         }
-
-        answer.push_back(seconds);
     }
+
 
     return answer;
 }
